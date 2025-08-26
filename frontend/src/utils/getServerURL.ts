194d8b521d3d214dbec35e6e97831470b8d4e53a
Ashtar Paniagua <ashtarcodes@gmail.com>
2025-08-26 01:07:00 -0400

    refactor getServerURL

--- frontend/src/utils/getServerURL.ts
+++ frontend/src/utils/getServerURL.ts
@@ -1,7 +1,10 @@
 export function getServerURL() {
   const fallback = "http://localhost:8000";
+
   if (typeof process !== "undefined" && process.env?.NEXT_PUBLIC_API_URL) {
-    return process.env.NEXT_PUBLIC_API_URL ?? fallback;
+    return process.env.IS_LOCAL === "true"
+      ? fallback
+      : process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`;
   }
 
   return fallback;
